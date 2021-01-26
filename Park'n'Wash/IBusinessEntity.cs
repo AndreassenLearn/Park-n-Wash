958700a44f0b3f6473b744cb81f4cf93e908f784
Simon Andreassen <simo531i@elevcampus.dk>
2021-01-26 13:09:05 +0100

    Added IBusinessEntity.cs.

---
+++ Park'n'Wash/IBusinessEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Park_n_Wash
+{
+    public interface IBusinessEntity
+    {
+        public int Id { get; }
+
+        /// <summary>
+        /// Check whether <see cref="IBusinessEntity"/> meets reqirements to be valid.
+        /// </summary>
+        /// <returns>True if valid; otherwise false.</returns>
+        bool Validate();
+    }
+}
