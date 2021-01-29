6a30cb2b7dbae49ddc3c9f4c53511a54e599390a
Simon Andreassen <simo531i@elevcampus.dk>
2021-01-29 13:57:10 +0100

    Added TicketController.cs and UserOption.cs. Moved UserInteraction to Common and started some experiments.

--- Park'n'Wash/TicketMachine.cs
+++ Park'n'Wash/TicketMachine.cs
@@ -1,4 +1,5 @@
-﻿using Park_n_Wash.Slot;
+﻿using Park_n_Wash.Common;
+using Park_n_Wash.Slot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,12 @@ namespace Park_n_Wash
     class TicketMachine
     {
         private SlotController _slotController;
-        private TicketRepository _ticketRepository;
+        private TicketController _ticketController;
 
         public TicketMachine()
         {
             _slotController = new SlotController();
-            _ticketRepository = new TicketRepository();
+            _ticketController = new TicketController();
         }
 
         /// <summary>
@@ -32,20 +33,42 @@ namespace Park_n_Wash
                 // Check in.
                 _slotController.GetAvailableSlots(out List<ISlot> normalSlots, out List<ISlot> handicapSlots, out List<ISlot> largeSlots, out List<ISlot> trailerSlots);
 
-                userOption = UserInteraction.SelectOption(
-                    new List<string> {
-                        "Regular slots: " + normalSlots.Count + " (" + _slotController.ElectricCountAndSort(normalSlots) + " of which offers electric charging)",
-                        "Handicap slots: " + handicapSlots.Count,
-                        "Large slots (bus/lorry): " + largeSlots.Count,
-                        "Trailer slots: " + trailerSlots.Count
-                    },
-                    "Available parking slots");
+                //userOption = UserInteraction.SelectOption(
+                //    new List<string> {
+                //        "Regular slots: " + normalSlots.Count + " (" + _slotController.ElectricCountAndSort(normalSlots) + " of which offers electric charging)",
+                //        "Handicap slots: " + handicapSlots.Count,
+                //        "Large slots (bus/lorry): " + largeSlots.Count,
+                //        "Trailer slots: " + trailerSlots.Count
+                //    },
+                //    "Available parking slots");
+
+                UserOption.OptionFunction del = TicketMachine.Test;
+
+                UserOption option = (UserOption)UserInteraction.SelectOption(new List<IPrintable>()
+                {
+                    new UserOption("First", new UserOption.OptionFunctionSlot(TicketMachine.TestSlot), normalSlots),
+                    new UserOption("Second", del)
+                },
+                "Title");
+                option.Execute();
             }
             else if (userOption == 1)
             {
                 // Check out.
-
+                
             }
+        }
+
+        public static void Test()
+        {
+            Console.WriteLine("Test");
+            System.Threading.Thread.Sleep(10000);
+        }
+
+        public static void TestSlot(List<ISlot> slots)
+        {
+            Console.WriteLine("TestSlot: \n" + slots[0].PrintableString());
+            System.Threading.Thread.Sleep(10000);
         }
     }
 }
---
+++ Park'n'Wash/UserOption.cs
@@ -0,0 +1,80 @@
+﻿using Park_n_Wash.Slot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Park_n_Wash.Common;
+
+namespace Park_n_Wash
+{
+    public class UserOption : IPrintable
+    {
+        private OptionFunction _optionFunction;
+        private OptionFunctionSlot _optionFunctionSlot;
+        
+        public delegate void OptionFunction();
+        public delegate void OptionFunctionSlot(List<ISlot> slot);
+
+        //private Dictionary<PriceFormats, string> _priceFormats = new Dictionary<PriceFormats, string>
+        //{
+        //    { PriceFormats.None, "" },
+        //    { PriceFormats.DKK, "DKK" },
+        //    { PriceFormats.DKKPrHour, "DKK/h" }
+        //};
+
+        /// <summary>
+        /// Defines formats for displaying a price.
+        /// </summary>
+        //public enum PriceFormats
+        //{
+        //    None,
+        //    DKK,
+        //    DKKPrHour
+        //}
+
+        public string Text { get; set; }
+        private List<ISlot> Slots { get; set; }
+        //public string SecondText { get; set; }
+        //public int? Availability { get; set; }
+        //public int? SecondAvailability { get; set; }
+        //public double? Price { get; set; }
+        //public PriceFormats PriceFormat { get; set; }
+
+        private UserOption(string text)
+        {
+            Text = text;
+        }
+        public UserOption(string text, OptionFunction optionFunction) : this(text)
+        {
+            _optionFunction = optionFunction;
+        }
+        public UserOption(string text, OptionFunctionSlot optionFunctionSlot, List<ISlot> slots) : this(text)
+        {
+            _optionFunctionSlot = optionFunctionSlot;
+            Slots = slots;
+        }
+
+        //public UserOption(string text, double? price = null, PriceFormats priceFormat = PriceFormats.None, int? availability = null, string secondText = "", int? secondAvailablity = null)
+        //{
+        //    Text = text;
+        //    Price = price;
+        //    PriceFormat = priceFormat;
+        //    Availability = availability;
+        //    SecondText = secondText;
+        //    SecondAvailability = secondAvailablity;
+        //}
+
+        public void Execute()
+        {
+            if (_optionFunction != null)
+                _optionFunction();
+            else if (_optionFunctionSlot != null)
+                _optionFunctionSlot(Slots);
+        }
+
+        public string PrintableString() =>
+            Text;
+            //$"{Text}{(Availability != null ? ": " + Availability : "")}{(SecondText != "" ? " (" + SecondText + (SecondAvailability != null ? ": " + SecondAvailability : "") + ")" : "" )}{(Price != null ? " - " + String.Format("{0:0.00}", Price) + " " + _priceFormats[PriceFormat] : "")}";
+    }
+}
