67a701d0306f43911150ea5e6c67e0b7bda8ca5a
Simon Andreassen <simo531i@elevcampus.dk>
2021-02-05 09:24:38 +0100

    Added CarWash.cs, IWash.cs, and IWashProcess.cs.

---
+++ Park'n'Wash/Wash/CarWash.cs
@@ -0,0 +1,19 @@
+﻿using Park_n_Wash.Ticket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Park_n_Wash.Wash
+{
+    class CarWash
+    {
+        public bool IsWashing { get; private set; }
+
+        public void RunWash(WashTicket ticket, IWash washType)
+        {
+
+        }
+    }
+}
---
+++ Park'n'Wash/Wash/IWash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Park_n_Wash.Wash
+{
+    interface IWash
+    {
+        public List<IWashProcess> WashProcesses { get; }
+    }
+}
